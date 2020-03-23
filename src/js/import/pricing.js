1efa66f6ac719c007bddea2384f9aa4e97064d82
Tim <ktn_96@icloud.com>
2020-03-23 13:08:26 +0200

    fix pricing

--- src/js/import/pricing.js
+++ src/js/import/pricing.js
@@ -1,3 +1,4 @@
+//// Page Pricing
 //currency selection
 $('.pricing__select li').on('click', function() {
   //active class
@@ -16,15 +17,39 @@ $('.pricing__select li').on('click', function() {
   //conversion
   let one = $(this).attr('data-one');
   let two = $(this).attr('data-two');
-  let three = $(this).attr('data-three');
   $('.pricing__tariff__block__title span').each(function(i, elem) {
     if(i === 0) {
       $(elem).text(one);
     } else if(i === 1) {
       $(elem).text(two);
-    } else if(i === 2) {
-      $(elem).text(three);
     }
+  });
+  
+}); 
+
+
+
+
+//// Page Whatsapp Landing
+//currency selection
+$('.whatsapp__pricing__select li').on('click', function() {
+  //active class
+  $('.whatsapp__pricing__select li').removeClass('active');
+  $(this).addClass('active');
+  //currency
+  if($(this).index() === 0) {
+    $('.whatsapp__pricing__block span').text('$');
+  } else if($(this).index() === 1) {
+    $('.whatsapp__pricing__block span').text('A$');
+  } else if($(this).index() === 2) {
+    $('.whatsapp__pricing__block span').text('£');
+  } else if($(this).index() === 3) {
+    $('.whatsapp__pricing__block span').text('₹');
+  }
+  //conversion
+  let one = $(this).attr('data-one');
+  $('.whatsapp__pricing__block strong').each(function(i, elem) {
+    $(elem).text(one);
   });
   
 }); 
