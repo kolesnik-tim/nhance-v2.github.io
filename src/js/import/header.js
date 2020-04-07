1c7e3a94b4fbb5a46ee017552e8b49c5d0c52617
Tim <ktn_96@icloud.com>
2020-04-07 14:45:25 +0300

    fix

--- src/js/import/header.js
+++ src/js/import/header.js
@@ -11,32 +11,35 @@ $('.menu__content .menu__item').on('click', function() {
   }
 });
 
+if($('div').hasClass('fullpage-wrapper')) {
 
-//header;
-var mywindow = $(window);
-var mypos = mywindow.scrollTop();
-var up = false;
-var newscroll;
-var banerInfo = $('.baner-info').innerHeight();
-$('.header').css({'top': banerInfo});
-mywindow.scroll(function() {
-  if(mywindow.scrollTop() > 50) {
-    $('.header').addClass('active');
-    newscroll = mywindow.scrollTop();
-    if (newscroll > mypos && !up) {
-      $('.header').stop().css({'top': '-200px'});
-      $('.slider__header').css({'top': banerInfo});
-      up = !up;
-    } else if(newscroll < mypos && up) {
-      $('.header').stop().css({'top': banerInfo});
-      $('.slider__header').css({'top': $('.header').innerHeight()});
-      up = !up;
+} else{
+  //header;
+  var mywindow = $(window);
+  var mypos = mywindow.scrollTop();
+  var up = false;
+  var newscroll;
+  var banerInfo = $('.baner-info').innerHeight();
+  $('.header').css({'top': banerInfo});
+  mywindow.scroll(function() {
+    if(mywindow.scrollTop() > 50) {
+      $('.header').addClass('active');
+      newscroll = mywindow.scrollTop();
+      if (newscroll > mypos && !up) {
+        $('.header').stop().css({'top': '-200px'});
+        $('.slider__header').css({'top': banerInfo});
+        up = !up;
+      } else if(newscroll < mypos && up) {
+        $('.header').stop().css({'top': banerInfo});
+        $('.slider__header').css({'top': $('.header').innerHeight()});
+        up = !up;
+      }
+      mypos = newscroll;
+    } else {
+      $('.header').removeClass('active');
     }
-    mypos = newscroll;
-  } else {
-    $('.header').removeClass('active');
-  }
-});
+  });
+}
 //dropdown
 $('.header__nav li').hover(function() {
   $(this).toggleClass('active');
