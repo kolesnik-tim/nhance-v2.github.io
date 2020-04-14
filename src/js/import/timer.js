9f6c4e2f73c2b98d40ed637e581f1306854570b7
Tim <ktn_96@icloud.com>
2020-04-14 12:27:40 +0300

    fix

--- src/js/import/timer.js
+++ src/js/import/timer.js
@@ -1,23 +1,26 @@
 if($('div').hasClass('webinar')) {
+
+
   setInterval(function() {
     var date = new Date();
-    var dateEnd = new Date(
+    var dateEnd = new Date(Date.UTC(
       $('.webinar__intro__clock').attr('year'), 
       $('.webinar__intro__clock').attr('month') - 1, 
       $('.webinar__intro__clock').attr('day'), 
       $('.webinar__intro__clock').attr('hour'), 
       $('.webinar__intro__clock').attr('minute'), 
       0
-    ); // // 2011 января 1, 00:00:00
-    if(dateEnd - date <= 0) {
+    )); // // 2011 января 1, 00:00:00
+    var dataEnd2 = dateEnd - date - 3600000;
+    if(dataEnd2 <= 0) {
 
     } else{
       var one_day = 1000 * 60 * 60 * 24;
       var one_hours = 1000 * 60 * 60;
       var one_minutes = 1000 * 60;
 
-      var day = Math.floor((dateEnd - date)/one_day);
-      var hoursResidue = (dateEnd - date)-(day*one_day);
+      var day = Math.floor((dataEnd2)/one_day);
+      var hoursResidue = (dataEnd2)-(day*one_day);
       var hours = Math.floor(hoursResidue / one_hours);
       var minutes = Math.floor(((hoursResidue-(hours*one_hours)) / one_minutes));
       var seconds = Math.floor((((hoursResidue-(hours*one_hours))-(minutes*one_minutes)) / 1000));
