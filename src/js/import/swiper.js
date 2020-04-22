ba1a6c135c5fbc03693b4df391df94ca49b38a59
Tim <ktn_96@icloud.com>
2020-04-22 13:38:43 +0300

    fix home page

--- src/js/import/swiper.js
+++ src/js/import/swiper.js
@@ -17,3 +17,23 @@ var swiper2 = new Swiper('.swiper-reviews', {
     }
   }
 });
+
+
+//home
+var swiper3 = new Swiper('.swiper-home', {
+  slidesPerView: 1,
+  // loop: true,
+  navigation: {
+    nextEl: '.swiper-home-next',
+    prevEl: '.swiper-home-prev',
+  },
+  // pagination: {
+  //   el: '.swiper-pagination-reviews',
+  //   clickable: true,
+  // },
+  // breakpoints: {
+  //   767: {
+  //     autoHeight: true,
+  //   }
+  // }
+});
