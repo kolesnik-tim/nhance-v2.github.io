750aa3fe3c1aad74982bf7492a359862d924f465
Tim <ktn_96@icloud.com>
2020-05-20 15:11:44 +0300

    fix

--- src/js/import/video.js
+++ src/js/import/video.js
@@ -2,18 +2,8 @@
 ////sorting
 //load
 setTimeout(function() {
-  $('.video__content h3 span').each(function(index, item) {
-    let text = $(item).text();
-    $('.video__content .col-xl-3').each(function(index, item) {
-      let blockItem = $(item);
-      console.log('123456');
-      $(item).find('li').each(function(index, item) {
-        if($(item).text() === text) {
-          $(blockItem).fadeIn();
-          
-        } else{}
-      });      
-    });
+  $('.video__aside__checkbox label').each(function(index, item) {
+    $('.video__content h3').append('<span>' + $(item).text() + '</span>');
   });
 }, 1000);
 
@@ -21,30 +11,58 @@ setTimeout(function() {
 $('.video__aside__checkbox label').on('click', function() {
   let text = $(this).text();
   if($(this).hasClass('checked')) {
-    $('.video__content .col-xl-3').each(function(index, item) {
-      let blockItem = $(item);
-      $(item).find('li').each(function(index, item) {
+    if(text === 'All') {
+      $('.video__content .col-xl-3').fadeOut();
+      $('.video__content h3 span').detach();
+      setTimeout(function() {
+        $('.video__aside__checkbox input').prop('checked', false);
+      }, 300);
+      $('.video__aside__checkbox label').removeClass('checked');
+      $('.video__aside__checkbox input').reoveAttr('checked');
+    } else{
+      $('.video__content .col-xl-3').each(function(index, item) {
+        let blockItem = $(item);
+        $(item).find('li').each(function(index, item) {
+          if($(item).text() === text) {
+            $(blockItem).fadeOut();
+          } else{}
+        });      
+      });
+      $(this).removeClass('checked');
+      $('.video__content h3 span').each(function(index, item) {
         if($(item).text() === text) {
-          $(blockItem).fadeOut();
-        } else{}
-      });      
-    });
-    $(this).removeClass('checked');
-    $('.video__content h3 span').each(function(index, item) {
-      if($(item).text() === text) {
-        $(item).remove();
-      }
-    });
+          $(item).remove();
+        }
+      });
+    }
+
+
+
   } else{
     $(this).addClass('checked');
-    $('.video__content h3').append('<span>' + text + '</span>');
-    $('.video__content .col-xl-3').each(function(index, item) {
-      let blockItem = $(item);
-      $(item).find('li').each(function(index, item) {
-        if($(item).text() === text) {
-          $(blockItem).fadeIn();
-        } else{}
-      });      
-    });
+    if(text === 'All') {
+      $('.video__content h3 span').detach();
+      $('.video__aside__checkbox label').addClass('checked');
+      $('.video__aside__checkbox label').each(function(index, item) {
+        $('.video__content h3').append('<span>' + $(item).text() + '</span>');
+      });
+      setTimeout(function() {
+        $('.video__aside__checkbox input').prop('checked', true);
+      }, 300);
+
+  
+
+      $('.video__content .col-xl-3').fadeIn();
+    } else{
+      $('.video__content h3').append('<span>' + text + '</span>');
+      $('.video__content .col-xl-3').each(function(index, item) {
+        let blockItem = $(item);
+        $(item).find('li').each(function(index, item) {
+          if($(item).text() === text) {
+            $(blockItem).fadeIn();
+          } else{}
+        });      
+      });
+    }
   }
 }); 
