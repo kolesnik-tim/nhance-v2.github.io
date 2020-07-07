1d22bd2f1ebd7039c91f6f16432158658cae3a86
Tim <ktn_96@icloud.com>
2020-07-07 15:50:01 +0300

    fix

--- src/js/import/pricing.js
+++ src/js/import/pricing.js
@@ -4,26 +4,15 @@ $('.pricing__select li').on('click', function() {
   //active class
   $('.pricing__select li').removeClass('active');
   $(this).addClass('active');
-  //currency
-  if($(this).index() === 0) {
-    $('.pricing__tariff__block__title strong').text('$');
-  } else if($(this).index() === 1) {
-    $('.pricing__tariff__block__title strong').text('A$');
-  } else if($(this).index() === 2) {
-    $('.pricing__tariff__block__title strong').text('£');
-  } else if($(this).index() === 3) {
-    $('.pricing__tariff__block__title strong').text('₹');
+  
+  if($(this).attr('data') === 'usd') {
+    $('.usd').css({'display': 'inline'});
+    $('.inr').css({'display': 'none'});
+  } else{
+    $('.usd').css({'display': 'none'});
+    $('.inr').css({'display': 'inline'});
   }
-  //conversion
-  let one = $(this).attr('data-one');
-  let two = $(this).attr('data-two');
-  $('.pricing__tariff__block__title span').each(function(i, elem) {
-    if(i === 0) {
-      $(elem).text(one);
-    } else if(i === 1) {
-      $(elem).text(two);
-    }
-  });
+  
   
 }); 
 
